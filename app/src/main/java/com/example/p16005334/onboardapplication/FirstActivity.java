c808173ca7f6c768eae68cd904c2c7491c4b560c
p16005334 <baptiste.pauletto@etu.univ-amu.fr>
2018-04-04 12:44:15 +0200

    Progression

--- app/src/main/java/com/example/p16005334/onboardapplication/FirstActivity.java
+++ app/src/main/java/com/example/p16005334/onboardapplication/FirstActivity.java
@@ -3,17 +3,27 @@ package com.example.p16005334.onboardapplication;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 public class FirstActivity extends AppCompatActivity {
-
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+//        mDrawerLayout = findViewById(R.id.drawer);
+//        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout,R.string.open,R.string.close);
+//        mDrawerLayout.addDrawerListener(mToggle);
+//        mToggle.syncState();
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
         boolean previouslyStarted = prefs.getBoolean(getString(R.string.pref_previously_started), false);
@@ -24,6 +34,12 @@ public class FirstActivity extends AppCompatActivity {
             showTutorial();
         }
     }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        if (mToggle.onOptionsItemSelected(item)) return true;
+//        return super.onOptionsItemSelected(item);
+//    }
 
     private void showTutorial() {
         Intent intent = MainActivity.makeIntent(FirstActivity.this);
